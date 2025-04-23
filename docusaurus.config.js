21fafc5aa9526cd4a502ba8eba24a5586c8201c1
sebastianvillegas25 <sebastianvillegas0628@gmail.com>
2025-04-23 16:25:37 -0500

    actualizacion de imagines logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
         title: 'Mi Manual',
         logo: {
           alt: 'Logo de Mi Manual',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            tutorial in less than - 5min ⏱️
           </Link>
         </div>
       </div>
